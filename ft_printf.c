991746f9fdd20e4e69f63c00dfb816edb7e51014
chanma <yunosuke.homma@icloud.com>
2022-06-11 12:36:10 +0900

    20220611 12:36

--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,50 @@
-int    ft_printf(const char *, ...)
+#include "ft_printf.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t 	i;
+
+	if (s == NULL)
+		return ;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+int	ft_printf(const char *format, ...)
+{
+
+	ft_putstr_fd((char *) format, 1);
+	return (0);
+}
+
+#ifdef TEST
+
+#include <stdio.h>
+
+#ifdef FT_PRINTF
+#define F(...) res = ft_printf(__VA_ARGS__);
+#else
+#define F(...) res = printf(__VA_ARGS__);
+#endif
+
+
+int main(void)
+{
+	int	res;
+	F("123\n");
+	return (0);
+}
+#endif
+
+#include <stdio.h>
+
+/*int main(void)
+{
+	ft_printf("hoge\n");
+	printf("hoge\n");
+	return (0);
+}*/
---
+++ ft_printf.h
@@ -0,0 +1,9 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+#endif
