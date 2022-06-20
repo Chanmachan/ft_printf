4ed8d2d8717102cb2f297a44726a2550a3ab623d
chanma <yunosuke.homma@icloud.com>
2022-06-20 13:38:40 +0900

    va_arg ptr

--- if_conv.c
+++ if_conv.c
@@ -3,85 +3,85 @@
 //
 #include "ft_printf.h"
 
-int 	if_d_or_i(va_list args)
+int 	if_d_or_i(va_list *args)
 {
 	int		rtn;
 	int 	conv;
 
 	rtn = 0;
-	conv = va_arg(args, int);
+	conv = va_arg(*args, int);
 	rtn += ft_putnbr(conv);
 	return (rtn);
 }
 
-int 	if_s(va_list args)
+int 	if_s(va_list *args)
 {
 	int 	rtn;
 	char	*s;
 
 	rtn = 0;
-	s = va_arg(args, char *);
+	s = va_arg(*args, char *);
 	rtn += ft_putstr_fd(s, 1);
 	return (rtn);
 }
 
-int 	if_c(va_list args)
+int 	if_c(va_list *args)
 {
 	int		rtn;
 	int 	conv;
 
 	rtn = 0;
-	conv = va_arg(args, int);
-	rtn += ft_putchar_fd((char)conv, 1);
+	conv = va_arg(*args, int);
+	rtn += ft_putchar_fd(conv, 1);
 	return (rtn);
 }
 
-int 	if_x(va_list args)
+int 	if_x(va_list *args)
 {
 	int 	rtn;
 	unsigned int conv;
 
 	rtn = 0;
-	conv = va_arg(args, unsigned int);
+	conv = va_arg(*args, unsigned int);
 	rtn += ft_put_conv_base_lower(conv);
 	return (rtn);
 }
 
-int 	if_X(va_list args)
+int 	if_X(va_list *args)
 {
 	int 	rtn;
 
 	unsigned int conv;
 	rtn = 0;
-	conv = va_arg(args, unsigned int);
+	conv = va_arg(*args, unsigned int);
 	rtn += ft_put_conv_base_upper(conv);
 	return (rtn);
 }
 
-int 	if_u(va_list args)
+int 	if_u(va_list *args)
 {
 	int 	rtn;
 
 	unsigned int u;
 	rtn = 0;
-	u = va_arg(args, unsigned int);
+	u = va_arg(*args, unsigned int);
 	rtn += ft_putnbr(u);
 	return (rtn);
 }
 
-int 	if_p(va_list args)
+int 	if_p(va_list *args)
 {
 	int 	rtn;
 	void	*p;
 
 	rtn = 0;
-	p = va_arg(args, void *);
+	p = va_arg(*args, void *);
 	rtn += ft_putstr_fd("0x", 1);
 	rtn += ft_put_conv_base_lower((long)p);
 	return (rtn);
 }
 
-int 	check_conv(char *str, va_list args)
+int 	check_conv(char *str, va_list *args)
 {
 	int 			rtn;
 	size_t 			i;
