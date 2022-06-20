004022d6dfdc38ab1670ac1ab760566e33d0febb
chanma <yunosuke.homma@icloud.com>
2022-06-20 13:38:08 +0900

    20220620 13:38

--- ft_printf.h
+++ ft_printf.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int		ft_printf(const char *format, ...);
 int 	ft_putchar_fd(char c, int fd);
@@ -12,13 +13,13 @@ int 	ft_putnbr(long args);
 int 	get_digit_hexa(long args);
 int 	ft_put_conv_base_lower(long args);
 int 	ft_put_conv_base_upper(long args);
-int 	check_conv(char *str, va_list args);
-int 	if_d_or_i(va_list args);
-int 	if_s(va_list args);
-int 	if_c(va_list args);
-int 	if_x(va_list args);
-int 	if_X(va_list args);
-int 	if_u(va_list args);
-int 	if_p(va_list args);
+int 	check_conv(char *str, va_list *args);
+int 	if_d_or_i(va_list *args);
+int 	if_s(va_list *args);
+int 	if_c(va_list *args);
+int 	if_x(va_list *args);
+int 	if_X(va_list *args);
+int 	if_u(va_list *args);
+int 	if_p(va_list *args);
 
 #endif
