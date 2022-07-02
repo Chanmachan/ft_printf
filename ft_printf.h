9e4e95b0a6b111093d1fb04f43892d6bcac2baa8
chanma <yunosuke.homma@icloud.com>
2022-07-02 20:31:57 +0900

    adjust norminette(not complete)

--- ft_printf.h
+++ ft_printf.h
@@ -1,9 +1,9 @@
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
 
 int				ft_printf(const char *format, ...);
 int 			ft_putchar_fd(char c, int fd);
