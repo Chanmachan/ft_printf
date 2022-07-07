fcfe262c863b987d66122fa605bbcefef2c00176
chanma <yunosuke.homma@icloud.com>
2022-07-08 00:30:37 +0900

    add

--- ft_printf.c
+++ ft_printf.c
@@ -10,8 +10,8 @@ int		ft_printf(const char *format, ...)
 	i = 0;
 	va_start(args, format);
 	str = (char *) format;
-	if (str[i] == '\0')
-		return (0);
+	if (str[i] == '\0' || str == NULL)
+		return (-1);
 	rtn = 0;
 	while (str[i] != '\0')
 	{
@@ -26,6 +26,19 @@ int		ft_printf(const char *format, ...)
 	}
 	va_end(args);
 	return (rtn);
+}
+
+#include <stdio.h>
+#include <libc.h>
+
+int main()
+{
+	int i = 0;
+	char *s = (char *)calloc(1, (size_t)INT_MAX + 10);
+	memset(s, 'a', (size_t)INT_MAX + 5);
+	i = ft_printf("%s\n", s);
+	printf("%d\n", i);
+	return (0);
 }
 
 /*#include <stdio.h>
