7c0e62e541f52c246d3c74c7c46aba7d1b241f9e
chanma <yunosuke.homma@icloud.com>
2022-07-12 02:41:04 +0900

    adjust norm

--- ft_printf.h
+++ ft_printf.h
@@ -7,20 +7,17 @@
 # include <limits.h>
 # include "../libft/libft.h"
 
-# include <stdio.h>
-
 int				ft_printf(const char *format, ...);
 int				read_args(char *str, va_list *args);
-int 			ft_putchar(char c);
-int 			ft_putstr(char *s, int count);
-int 			ft_putnbr(long args);
-unsigned int 	ft_put_conv_base(unsigned long long args, char x_X);
-int 			check_conv(char *str, va_list *args, int count);
-int 			if_d_or_i(va_list *args);
-int 			if_s(va_list *args, int count);
-int 			if_c(va_list *args);
-int				if_x(va_list *args, char x_X);
-int 			if_u(va_list *args);
-int 			if_p(va_list *args, int count);;
+int				ft_putchar(char c);
+int				ft_putstr(char *s, int count);
+int				ft_putnbr(long args);
+unsigned int	ft_put_conv_base(unsigned long long args, char x_X);
+int				check_conv(char *str, va_list *args, int count);
+int				if_d_or_i(va_list *args);
+int				if_s(va_list *args, int count);
+int				if_c(va_list *args);
+int				if_u(va_list *args);
+int				if_p_x(va_list *args, int count, char c);
 
 #endif
