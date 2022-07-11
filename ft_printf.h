fdcdf95897fdbd3c284cb1620b96e9dc10204007
chanma <yunosuke.homma@icloud.com>
2022-07-12 02:49:02 +0900

    delete func

--- ft_printf.h
+++ ft_printf.h
@@ -8,12 +8,10 @@
 # include "../libft/libft.h"
 
 int				ft_printf(const char *format, ...);
-int				read_args(char *str, va_list *args);
 int				ft_putchar(char c);
 int				ft_putstr(char *s, int count);
 int				ft_putnbr(long args);
 unsigned int	ft_put_conv_base(unsigned long long args, char x_X);
-int				check_conv(char *str, va_list *args, int count);
 int				if_d_or_i(va_list *args);
 int				if_s(va_list *args, int count);
 int				if_c(va_list *args);
