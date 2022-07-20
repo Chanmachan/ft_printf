6ddcbbe0e57921090f8ab305ceeaee0d560eeba1
chanma <yunosuke.homma@icloud.com>
2022-07-20 17:41:04 +0900

    adjust norminette

--- ft_printf.h
+++ ft_printf.h
@@ -25,7 +25,8 @@ int				ft_printf(const char *format, ...);
 long long		ft_putchar(char c, long long count);
 long long		ft_putstr(char *s, long long count);
 long long		ft_putnbr(long args, long long count);
-long long		ft_put_conv_base(unsigned long long args, char x_X, long long count);
+long long		ft_put_conv_base(unsigned long long args, \
+									char x_X, long long count);
 long long		if_d_or_i(va_list *args, long long count);
 long long		if_s(va_list *args, long long count);
 long long		if_c(va_list *args, long long count);
