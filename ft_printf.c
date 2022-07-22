795a2499a0a08ba83c95c8ed5ef98c32cd8aee21
chanma <yunosuke.homma@icloud.com>
2022-07-23 04:19:32 +0900

    change

--- ft_printf.c
+++ ft_printf.c
@@ -67,6 +67,20 @@ int	ft_printf(const char *format, ...)
 }
 
 /*#include <libc.h>
+#include "ft_printf.h"
+
+int main()
+{
+	int i = 0;
+	char *s = (char *)calloc(1, (size_t)INT_MAX + 10);
+	memset(s, 'a', (size_t)INT_MAX + 5);
+	i = ft_printf("%s\n", s);
+	printf("%d\n", i);
+	free(s);
+	return (0);
+}*/
+
+/*#include <libc.h>
 
 int main()
 {
